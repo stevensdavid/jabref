430963a04b60a686adedae4629101cb70219eeb8
Tommy <tommy.s.samuelsson@gmail.com>
2020-02-27 13:09:58 +0100

    [feat] Add test for the StringChangeNextWord class
    This commit adds more test case for the StringChangeNextWord class.
    
    [issue: #9]

--- src/test/java/org/jabref/logic/util/strings/StringChangeNextWordTest.java
+++ src/test/java/org/jabref/logic/util/strings/StringChangeNextWordTest.java
@@ -24,4 +24,41 @@ public class StringChangeNextWordTest {
         String textOutput = new StringChangeNextWord().editNextWordUpperCase(pos, textInput);
         assertEquals(textOutput, whatTextShouldBe);
     }
+
+    @Test
+    public void editNextWordLowerCaseEditsTheNextWord() {
+        int pos = 3;
+        String textInput = "hello HELLO";
+        String whatTextShouldBe = "hello hello";
+        String textOutput = new StringChangeNextWord().editNextWordLowerCase(pos, textInput);
+        assertEquals(textOutput, whatTextShouldBe);
+    }
+
+    @Test
+    public void editNextWordToEmptyRemovesFromPostionUpToNextWord() {
+        int pos = 3;
+        String textInput = "hello hello";
+        String whatTextShouldBe = "hel hello";
+        String textOutput = new StringChangeNextWord().editNextWordToEmpty(pos, textInput);
+        assertEquals(textOutput, whatTextShouldBe);
+    }
+
+    @Test
+    public void editNextWordToEmptyRemovesNextWordIfPositionIsInSpace() {
+        int pos = 5;
+        String textInput = "hello person";
+        String whatTextShouldBe = "person";
+        String textOutput = new StringChangeNextWord().editNextWordToEmpty(pos, textInput);
+        assertEquals(textOutput, whatTextShouldBe);
+    }
+
+    @Test
+    public void getNumOfSpaceTwoWordOneSpace() {
+        int pos = 8;
+        String textInput = "hello person";
+        int whatNumOfSpaceShouldBe = 1;
+        int numOfSpace = new StringChangeNextWord().getNumOfSpace(pos, textInput);
+        assertEquals(numOfSpace, whatNumOfSpaceShouldBe);
+    }
+
 }
